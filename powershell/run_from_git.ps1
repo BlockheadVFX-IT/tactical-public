9d7b2eaebf8353486a0b4ecfe47af20530091c9e
Sagar Kandel Admin <sagar.kandel.admin@blockheadvfx.com>
2024-01-19 12:01:55 +1100

    new code

--- powershell/run_from_git.ps1
+++ powershell/run_from_git.ps1
@@ -4,11 +4,11 @@ function RunFromGit
         [Parameter(Mandatory = $true)][string]$script, # Path of file in GitHub repo
         $outfile, # File to execute (probably same as above sans dirs)
         $automation_name, # Used for temp dir names
-        [string]$github_api_url = 'https://api.github.com/repos/BlockheadVFX-IT/boilerplates/contents', # GitHub API URL for repository contents
-        [string]$github_raw_url = 'https://raw.githubusercontent.com/BlockheadVFX-IT', # Raw GitHub URL for scripts
-        [bool]$load_helpers = $true, # Flag to determine whether to load helper scripts
-        [bool]$user_mode = $false, # If running as logged on user instead of system user, will change working dir to $env:LOCALAPPDATA
-        [string]$pub_branch = 'main' # Used to swap to different test branches if needed
+        [string]$github_api_url = 'https://api.github.com/repos/BlockheadVFX-IT/boilerplates/contents', # If you are using a proxy change this
+        [string]$github_raw_url = 'https://raw.githubusercontent.com/BlockheadVFX-IT', # If you are using a proxy change this
+        [bool]$load_helpers = $true,
+        [bool]$user_mode = $false, # If running as a logged-on user instead of the system user, will change working dir to $env:LOCALAPPDATA
+        [string]$pub_branch = 'main' # used to swap to different test branches if you want
     )
 
     # Save the current working directory
@@ -17,8 +17,6 @@ function RunFromGit
     # Load helper scripts if required
     if ($load_helpers)
     {
-        # If you want to add more helpers, include their names here and upload them to the 
-        # powershell/helpers/ folder for the public GitHub repo
         $helper_files = @('create_shortcut.ps1', 'check_installed.ps1', 'set_env_var.ps1', 'set_reg_key.ps1', 'uninstall_program.ps1')
         $base_url = "$github_raw_url/tactical-public/$pub_branch/powershell/helpers"
 
@@ -29,27 +27,24 @@ function RunFromGit
         }
     }
 
-    # Determine the working directory based on user mode
+    # Set the appropriate temp directory based on user mode
     if ($user_mode)
     {
-        $working_dir = "$env:LOCALAPPDATA\Temp" # In user mode, ProgramData is not writable by most users
+        $ninja_dir = "$env:LOCALAPPDATA\Temp" # In user mode, ProgramData is not writable by most users
     }
     else
     {
-        $working_dir = 'C:\ProgramData\TacticalRMM' # Otherwise use this directory
+        $ninja_dir = 'C:\ProgramData\TacticalRMM' # Otherwise use this dir
     }
 
-    # Get the personal access token (PAT) from a private S3 bucket
-    Write-Host 'Getting personal access token from Sagars private S3 bucket.........'
+    # Get the personal access token (PAT) from S3 to access the private repo
+    Write-Host 'Getting personal access token from Sagar''s private S3 bucket...'
     $pat_url_b64 = 'aHR0cHM6Ly90YW5nZWxvYnVja2V0bmluamEuczMuYXAtc291dGhlYXN0LTIuYW1hem9uYXdzLmNvbS90cm1tX2dpdGh1Yl9wYXQucGF0'
     $pat_url = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($pat_url_b64))
     $pat = Invoke-WebRequest -Uri $pat_url -UseBasicParsing | Select-Object -ExpandProperty Content
     $pat = [Text.Encoding]::UTF8.GetString($pat)
 
-    # Display PAT and GitHub API URL for verification
-    echo "Personal Access Token (PAT): $pat"
-    echo "GitHub API URL: $github_api_url"
-
+    # Check whether we are getting a file or a folder
     # Define headers for GitHub API request
     $headers = @{
         'Accept'               = 'application/vnd.github.v3.object'
@@ -67,66 +62,53 @@ function RunFromGit
     # Convert the JSON response to a PowerShell object
     $responseObject = $response | ConvertFrom-Json
 
-    # Extract the script list based on the GitHub response
-    $script_list = @() # Treat as an array even if we only end up with one script at a time
+    # Set up temp dirs and download the script
+    $outfile = Split-Path -Path $script -Leaf
+    $automation_name = Format-InvalidPathCharacters -path $outfile
+    New-Item -ItemType Directory "$ninja_dir\$automation_name" -Force | Out-Null
+    Set-Location "$ninja_dir\$automation_name"
 
-    if ($responseObject.type -eq 'file')
+    Write-Host "Getting $script from GitHub..."
+    Invoke-WebRequest -Uri $uri -Headers $headers -OutFile $outfile -UseBasicParsing
+
+    if (Test-Path $outfile)
     {
-        $script_list += $responseObject.path
+        Write-Host "$outfile downloaded successfully"
+    }
+    else
+    {
+        Write-Host "$outfile not downloaded"
     }
 
-    # Iterate through each script in the list
-    foreach ($script in $script_list)
+    # Run the script
+    $process_error = $false
+    try
     {
-        # Set up file and directory names
-        $outfile = Split-Path -Path $script -Leaf
-        $automation_name = Format-InvalidPathCharacters -path $outfile
-
-        # Set up temp dirs
-        New-Item -ItemType Directory "$working_dir\$automation_name" -Force | Out-Null
-        Set-Location "$working_dir\$automation_name"
-
-        # Download script from GitHub
-        Write-Host "Getting $script from GitHub..."
-        Invoke-WebRequest -Uri "$github_api_url/$([System.Uri]::EscapeDataString($script))" -Headers $headers -OutFile $outfile -UseBasicParsing
-
-        # Check if the script was downloaded successfully
-        if (Test-Path $outfile)
-        {
-            Write-Host "$outfile downloaded successfully"
-        }
-        else
-        {
-            Write-Host "$outfile not downloaded"
-        }
-
-        # Run the downloaded script
-        $process_error = $false
-        try
-        {
-            Write-Host "Running $outfile ..."
-            & ".\$outfile" 2>&1 | Out-String
-            $result = $LASTEXITCODE
-            Write-Host "$outfile done, cleaning up..."
-        }
-        catch
-        {
-            # Capture any errors during script execution
-            $process_error = $_.Exception 
-        }
-
-        # Clean up temporary directories
-        Set-Location "$working_dir"
-        Remove-Item "$working_dir\$automation_name" -Force -Recurse
-
-        # Display the result of script execution
-        Write-Host $result
+        Write-Host "Running $outfile ..."
+        & ".\$outfile" 2>&1 | Out-String
+        $result = $LASTEXITCODE
+        Write-Host "$outfile done, cleaning up..."
+    }
+    catch
+    {
+        $process_error = $_.Exception
     }
 
-    # Restore the previous working directory
+    # Clean up
+    Set-Location "$ninja_dir"
+    Remove-Item "$ninja_dir\$automation_name" -Force -Recurse
+
+    if (Test-Path "$ninja_dir\$automation_name")
+    {
+        Write-Host "Failed to clean up $ninja_dir\$automation_name"
+    }
+    else
+    {
+        Write-Host "Cleaned up $ninja_dir\$automation_name"
+    }
+
     Set-Location $prev_cwd
 
-    # Throw an error if there was an error during script execution
     if ($process_error)
     {
         throw $process_error
