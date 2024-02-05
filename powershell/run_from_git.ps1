f697ae0dba6ac4a88fbae35fb472d64f368b00b0
Sagar Kandel Admin <sagar.kandel.admin@blockheadvfx.com>
2024-02-05 11:33:20 +1100

    tactial-git pat url updated

--- powershell/run_from_git.ps1
+++ powershell/run_from_git.ps1
@@ -42,7 +42,8 @@ function RunFromGit
     # Start by getting the PAT from S3 to access our private repo
     Write-Host 'Getting personal access token from S3...'
     # pat URL encoded with b64 here just to avoid getting grabbed by scrapers
-    $pat_url_b64 = 'aHR0cHM6Ly90YW5nZWxvYnVja2V0bmluamEuczMuYXAtc291dGhlYXN0LTIuYW1hem9uYXdzLmNvbS90cm1tX2dpdGh1Yl9wYXQucGF0'    ##converted so that RL could be hidden 
+    #$pat_url_b64 = 'aHR0cHM6Ly90YW5nZWxvYnVja2V0bmluamEuczMuYXAtc291dGhlYXN0LTIuYW1hem9uYXdzLmNvbS90cm1tX2dpdGh1Yl9wYXQucGF0'    ##converted so that RL could be hidden 
+    $pat_url_b64 = 'aHR0cHM6Ly9ha2wwOHZtaW5pbzAxLmJsb2NraGVhZHZmeC5jb20vdHJtbS1jcmVkZW50aWFscy9HaXRodWJfVG9rZW4vdHJtbV9naXRodWJfcGF0LnBhdA=='
     $pat_url = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($pat_url_b64))          ##command to decode the above random string
     $pat = Invoke-WebRequest -Uri $pat_url -UseBasicParsing | Select-Object -ExpandProperty Content              ##downloads the PAT file form the URL and gets contents of that file 
     $pat = [Text.Encoding]::UTF8.GetString($pat)
